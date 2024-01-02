b2c29311636e383a2d56cebd9aef316140b03a80
Tianyi Xu <896408725@qq.com>
2024-01-02 19:31:26 +0800

    NavBar_xty_0102

--- UI/app/src/component/NavBar.js
+++ UI/app/src/component/NavBar.js
@@ -187,6 +187,36 @@ const NavBar = (props) => {
   var optionsOBJ = OBJFiles.map((item) => {
     return {label: item, value: item};
   });
+  const [CircleVisible, setCircleVisible] = useState(false);
+  const [CircleRadius, setCircleRadius] = useState(1);
+
+  const [RingVisible, setRingVisible] = useState(false);
+  const [RingRadius1, setRingRadius1] = useState(1);
+  const [RingRadius2, setRingRadius2] = useState(2);
+
+  const [ConeVisible, setConeVisible] = useState(false);
+  const [ConeRadius, setConeRadius] = useState(1);
+  const [ConeHeight, setConeHeight] = useState(2);
+
+  const [CylinderVisible, setCylinderVisible] = useState(false);
+  const [CylinderRadius, setCylinderRadius] = useState(1);
+  const [CylinderHeight, setCylinderHeight] = useState(2);
+
+  const [PyramidVisible, setPyramidVisible] = useState(false);
+  const [PyramidDegree, setPyramidDegree] = useState(1);
+  const [PyramidHeight, setPyramidHeight] = useState(2);
+  const [PyramidRadius, setPyramidRadius] = useState(1);
+
+  const [prismVisible, setPrismVisible] = useState(false);
+  const [prismDegree, setPrismDegree] = useState(1);
+  const [prismHeight, setPrismHeight] = useState(1);
+  const [prismRadius, setPrismRadius] = useState(1);
+
+  const [prismoidVisible, setPrismoidVisible] = useState(false);
+  const [prismoidDegree, setPrismoidDegree] = useState(1);
+  const [prismoidHeight, setPrismoidHeight] = useState(1);
+  const [prismoidRadius, setPrismoidRadius] = useState(1);
+
   const [NURBSVisible, setNURBSVisible] = useState(false);
   const [NURBSUControlNum, setNURBSUControlNum] = useState(5);
   const [NURBSVControlNum, setNURBSVControlNum] = useState(5);
@@ -212,7 +242,31 @@ const NavBar = (props) => {
         CreateSquare();
         break;
       case 'Circle':
-        CreateCircle();
+        setCircleVisible(true);
+        break;
+      case 'Sphere':
+        props.createSphere();
+        break;
+      case 'Rectangle':
+        props.createRectangle();
+        break;
+      case 'Ring':
+        setRingVisible(true);
+        break;
+      case 'Cone':
+        setConeVisible(true);
+        break;
+      case 'Cylinder':
+        setCylinderVisible(true);
+        break;
+      case 'Pyramid':
+        setPyramidVisible(true);
+        break;
+      case 'Prism':
+        setPrismVisible(true);
+        break;
+      case 'Prismoid':
+        setPrismoidVisible(true);
         break;
       case 'NURBS':
         setNURBSVisible(true);
@@ -232,6 +286,123 @@ const NavBar = (props) => {
   return (
     <>
      <Menu onClick={onClick} mode="horizontal" items={items} />
+
+     <Modal title="Circle" open={CircleVisible} onClose={() => setCircleVisible(false)} 
+        onOk={() => {
+          props.createCircle(CircleRadius);
+          setCircleVisible(false);
+        }}
+       onCancel={() => setCircleVisible(false)}>
+      {/*输入参数为半径 */}
+        <InputNumber 
+        addonBefore="半径"
+        min={0} max={10} defaultValue={1} onChange={(value) => setCircleRadius(value)} />
+      </Modal>
+
+      <Modal title="Ring" open={RingVisible} onClose={() => setRingVisible(false)}
+        onOk={() => {
+          props.createRing(RingRadius1, RingRadius2);
+          setRingVisible(false);
+        }}
+        onCancel={() => setRingVisible(false)}>
+        <Space direction="vertical">
+        <InputNumber
+          addonBefore="内半径"
+          min={0} max={10} defaultValue={1} onChange={(value) => setRingRadius1(value)} />
+        <InputNumber
+          addonBefore="外半径"
+          min={0} max={10} defaultValue={2} onChange={(value) => setRingRadius2(value)} />
+        </Space>
+      </Modal>
+      <Modal title="Cone" open={ConeVisible} onClose={() => setConeVisible(false)}
+        onOk={() => {
+          props.createCone(ConeRadius, ConeHeight);
+          setConeVisible(false);
+        }}
+        onCancel={() => setConeVisible(false)}>
+        <Space direction="vertical">
+        <InputNumber
+          addonBefore="半径"
+          min={0} max={10} defaultValue={1} onChange={(value) => setConeRadius(value)} />
+        <InputNumber
+          addonBefore="高度"
+          min={0} max={10} defaultValue={2} onChange={(value) => setConeHeight(value)} />
+        </Space>
+      </Modal>
+      
+      <Modal title="Cylinder" open={CylinderVisible} onClose={() => setCylinderVisible(false)}
+        onOk={() => {
+          props.createConecylinder(CylinderRadius, CylinderHeight);
+          setCylinderVisible(false);
+        }}
+        onCancel={() => setCylinderVisible(false)}>
+        <Space direction="vertical">
+          <InputNumber
+            addonBefore="半径"
+            min={0} max={10} defaultValue={1} onChange={(value) => setCylinderRadius(value)} />
+          <InputNumber
+            addonBefore="高度"
+            min={0} max={10} defaultValue={2} onChange={(value) => setCylinderHeight(value)} />
+        </Space>
+      </Modal>
+
+      <Modal title="Pyramid" open={PyramidVisible} onClose={() => setPyramidVisible(false)}
+        onOk={() => {
+          props.createPyramid(PyramidDegree, PyramidHeight, PyramidRadius);
+          setPyramidVisible(false);
+        }}
+        onCancel={() => setPyramidVisible(false)}>
+        <Space direction="vertical">
+          <InputNumber
+            addonBefore="度数"
+            min={0} max={10} defaultValue={1} onChange={(value) => setPyramidDegree(value)} />
+          <InputNumber
+            addonBefore="高度"
+            min={0} max={10} defaultValue={2} onChange={(value) => setPyramidHeight(value)} />
+          <InputNumber
+            addonBefore="半径"
+            min={0} max={10} defaultValue={1} onChange={(value) => setPyramidRadius(value)} />
+        </Space>
+      </Modal>
+
+      <Modal title="Prism" open={prismVisible} onClose={() => setPrismVisible(false)}
+        onOk={() => {
+          props.createPrism(prismDegree, prismHeight, prismRadius);
+          setPrismVisible(false);
+        }}
+        onCancel={() => setPrismVisible(false)}>
+        <Space direction="vertical">
+          <InputNumber
+            addonBefore="度数"
+            min={0} max={10} defaultValue={1} onChange={(value) => setPrismDegree(value)} />
+          <InputNumber
+            addonBefore="高度"
+            min={0} max={10} defaultValue={1} onChange={(value) => setPrismHeight(value)} />
+          <InputNumber
+            addonBefore="半径"
+            min={0} max={10} defaultValue={1} onChange={(value) => setPrismRadius(value)} />
+        </Space>
+      </Modal>
+
+      <Modal title="Prismoid" open={prismoidVisible} onClose={() => setPrismoidVisible(false)}
+        onOk={() => {
+          props.createPrismoid(prismoidDegree, prismoidHeight, prismoidRadius);
+          setPrismoidVisible(false);
+        }}
+        onCancel={() => setPrismoidVisible(false)}>
+        <Space direction="vertical">
+          <InputNumber
+            addonBefore="度数"
+            min={0} max={10} defaultValue={1} onChange={(value) => setPrismoidDegree(value)} />
+          <InputNumber
+            addonBefore="高度"
+            min={0} max={10} defaultValue={1} onChange={(value) => setPrismoidHeight(value)} />
+          <InputNumber
+            addonBefore="半径"
+            min={0} max={10} defaultValue={1} onChange={(value) => setPrismoidRadius(value)} />
+        </Space>
+      </Modal>
+
      <Modal title="NURBS" open={NURBSVisible} onClose={() => setNURBSVisible(false)} 
         onOk={() => {
           props.CreateNURBS(NURBSUControlNum, NURBSVControlNum, NURBSDeg);
